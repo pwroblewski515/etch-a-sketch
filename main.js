98d4aade2824643ed8e437d3a5f17b3310801765
Paul Wroblewski <p.wroblewski515@gmail.com>
2020-12-13 18:22:27 -0600

    add prompt to determine size of sketchpad on demand

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const sketchPad = document.querySelector(".grid-container");
 const resetButton = document.querySelector("button");
-let numColumns = 32;
+let numColumns = getNumColumns();
+updateNumColumnsInGrid(numColumns);
 let mouseDown = false;
+
+function getNumColumns(){
+    let numColumns = prompt("Please enter a number of columns: ");
+    while (true){
+        if (numColumns < 100) return numColumns;
+        numColumns = prompt("Please enter a number less than 100");
+    }
+}
+
+function updateNumColumnsInGrid(numColumns){
+    sketchPad.style.gridTemplateColumns = `repeat(${numColumns},auto)`;
+    sketchPad.style.gridTemplateRows = `repeat(${numColumns},auto)`;
+}
 
 for (let i = 0; i < numColumns*numColumns; i++){
     sketchPad.appendChild(document.createElement('div'));
